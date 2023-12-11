8bc90e558032e7f72ae4c422814eecb674f889f0
Mattias Tengbrand <mattias.tengbrand@sveaskog.se>
2023-12-11 06:36:02 +0100

    Skapat kort av resultatet

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -27,19 +27,56 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair =appState.current;
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+          Text('A random greate idea:'),
+          BigCard(pair: pair),
+          ElevatedButton(
+            onPressed: () {
+              //print('button pressed!');
+              appState.getNext();
+            },
+            child: Text('Next'),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asPascalCase, style: style),
       ),
     );
   }
